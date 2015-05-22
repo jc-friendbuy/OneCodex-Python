1bb2fb88e1dafd1348275a4c8e87bc2dd8afd275
Juan Carlos Coto <jccotou@gmail.com>
2015-05-21 22:08:40 -0600

    Adds function to get resource url for Analysis

--- src/v0/analyses/Analysis.py
+++ src/v0/analyses/Analysis.py
@@ -2,11 +2,16 @@
 This class contains the functionality of a specific One Codex analysis, related to a sample.
 """
 
+from purl import URL, expand
+from config import Configuration
+
 class Analysis(object):
     """
     This class allows for interaction with One Codex Analysis API objects, particularly retrieving
     analysis result information for a specified analysis.
     """
+
+    RESOURCE_PATH = "analyses/{id}"
 
     def __init__(self, the_id):
         """
@@ -26,3 +31,7 @@ class Analysis(object):
 
     def save_raw_data_to_path(self, out_path):
         pass
+
+    def _get_resource_url(self):
+        url = URL(Configuration.BASE_API_URL) \
+         .path(expand(Analysis.RESOURCE_PATH, {"id": self._id}))
