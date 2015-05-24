9e5e4e93c9d9c17f6972a4bc64d0d10a0a065c02
Juan Carlos Coto <jccotou@gmail.com>
2015-05-23 23:46:12 -0600

    Adds the API_KEY setting to the Configuration class.

--- src/v0/config.py
+++ src/v0/config.py
@@ -1,3 +1,4 @@
-
 class Configuration(object):
-	BASE_API_URL = "https://beta.onecodex.com/api/v0/"
+
+    API_KEY = "f2c79541cbe848e09a373e0357edf8e6"
+    BASE_API_URL = "https://beta.onecodex.com/api/v0/"
