7ea693b6fc347a505186c2bcbae49bb1798362ef
Juan Carlos Coto <jccotou@gmail.com>
2015-05-25 02:06:04 -0600

    Adds test for checking Analysis.get_table() returns a list and refactors some test names

--- test/v0/analyses/TestAnalysis.py
+++ test/v0/analyses/TestAnalysis.py
@@ -1,23 +1,29 @@
 """
 This is a test file for the src/v0/analyses/Analysis class.
 """
+
+from __future__ import print_function
 import unittest
 from v0.analyses.Analysis import Analysis
 
 
 class TestAnalysis(unittest.TestCase):
 
-    def test_get_resource_url(self):
+    def test_get_resource_url_is_correct(self):
         analysis = Analysis("test_id")
         self.assertEqual(
             analysis._get_resource_url(),
             u"https://beta.onecodex.com/api/v0/analyses/test_id"
         )
 
-    def test_get_action_url(self):
+    def test_get_action_url_is_correct(self):
         analysis = Analysis("test_id")
         self.assertEqual(
             analysis._get_action_url("table"),
             u"https://beta.onecodex.com/api/v0/analyses/test_id/table"
         )
 
+    def test_get_table_results_is_instance_list(self):
+        analysis = Analysis("3186542a03c74205")
+        table_results = analysis.get_table()
+        self.assertIsInstance(table_results, list)
