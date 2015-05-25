b49b3ec285a919017de1d3b1dfb12836b73d7b16
Juan Carlos Coto <jccotou@gmail.com>
2015-05-24 23:05:00 -0600

    Adds first test for Analysis class, generation of resource URL.

---
+++ test/v0/analyses/TestAnalysis.py
@@ -0,0 +1,16 @@
+"""
+This is a test file for the src/v0/analyses/Analysis class.
+"""
+import unittest
+from v0.analyses.Analysis import Analysis
+
+
+class TestAnalysis(unittest.TestCase):
+
+    def test_get_resource_url(self):
+        analysis = Analysis("test_id")
+        self.assertEqual(
+            analysis._get_resource_url(),
+            u"https://beta.onecodex.com/api/v0/analyses/test_id"
+        )
+
