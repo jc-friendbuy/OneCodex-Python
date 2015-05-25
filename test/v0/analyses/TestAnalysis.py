9fa645bb73a4e1d28dc7a029f47681a0f7bb37e5
Juan Carlos Coto <jccotou@gmail.com>
2015-05-25 02:08:07 -0600

    Refactor test names again (go back to previous version and simplify get_table

--- test/v0/analyses/TestAnalysis.py
+++ test/v0/analyses/TestAnalysis.py
@@ -9,21 +9,23 @@ from v0.analyses.Analysis import Analysis
 
 class TestAnalysis(unittest.TestCase):
 
-    def test_get_resource_url_is_correct(self):
+    def test_get_resource_url(self):
         analysis = Analysis("test_id")
         self.assertEqual(
             analysis._get_resource_url(),
             u"https://beta.onecodex.com/api/v0/analyses/test_id"
         )
 
-    def test_get_action_url_is_correct(self):
+    def test_get_action_url(self):
         analysis = Analysis("test_id")
         self.assertEqual(
             analysis._get_action_url("table"),
             u"https://beta.onecodex.com/api/v0/analyses/test_id/table"
         )
 
-    def test_get_table_results_is_instance_list(self):
+    def test_get_table(self):
         analysis = Analysis("3186542a03c74205")
         table_results = analysis.get_table()
         self.assertIsInstance(table_results, list)
+
+    # ADD MORE TESTS FOR GET TABLE
