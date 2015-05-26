0b00dcbda5f513cc4e76e98292687c1c551e2aeb
Juan Carlos Coto <jccotou@gmail.com>
2015-05-26 11:51:31 -0600

    Adds tests for save_raw_data_to_path.
    Tests for the output file are created, which test the file size, creation date and md5 digest (in order to check if contents have changed).

--- test/v0/analyses/TestAnalysis.py
+++ test/v0/analyses/TestAnalysis.py
@@ -3,6 +3,8 @@ This is a test file for the src/v0/analyses/Analysis class.
 """
 
 from __future__ import print_function
+from os import path, stat
+from hashlib import md5
 import unittest
 from v0.analyses.Analysis import Analysis
 
@@ -27,5 +29,17 @@ class TestAnalysis(unittest.TestCase):
         analysis = Analysis("3186542a03c74205")
         table_results = analysis.get_table()
         self.assertIsInstance(table_results, list)
+        # TODO: add more tests
+
+    def test_save_raw_data_to_path(self):
+        destination_path = "/Users/jc/Desktop/ocanalysis.tsv.gz"
+        analysis = Analysis("3186542a03c74205")
+        analysis.save_raw_data_to_path(destination_path)
+        self.assertTrue(path.isfile(destination_path))
+        file_stat_info = stat(destination_path)
+        self.assertTrue(file_stat_info.st_size, 93735)
+        self.assertTrue(file_stat_info.st_birthtime, 1432661815)
+        md5_digest = md5(open(destination_path, "rb").read()).hexdigest()
+        self.assertTrue(md5_digest, "ef636300ebf69d82720f335842efd2ba")
 
     # ADD MORE TESTS FOR GET TABLE
