39ac90ffed5eab9be5b241bfb95b3452fe1a470b
Juan Carlos Coto <jccotou@gmail.com>
2015-06-08 17:28:33 -0600

    Modify Analysis to receive One Codex data attributes as parameters.
    This commit adds data attributes for the Analysis class, so that more information can be extracted from it.
    
    This is in preparation for the AnalysisRepository class, which retrieves data from the system and populates
    Analysis instances with it.

--- src/v0/analyses/Analysis.py
+++ src/v0/analyses/Analysis.py
@@ -14,18 +14,36 @@ class Analysis(object):
 
     _url_builder = OneCodexAPIURLBuilder(u"analyses")
 
-    def __init__(self, the_id):
+    def __init__(self, the_id, status, number_reads, proportion_mapped, reference_id,
+                 reference_name, sample_id, sample_filename):
         """
-        Create a new instance of analysis with the provided id.
+        Create a new instance of Analysis.
+        :param the_id: The ID in the One Codex system.
+        :param status: The current status of the analysis ("Success", "Pending", or "Failed").
+        :param number_reads: Number of reads or contigs in the sample.
+        :param proportion_mapped: Proportion of sample reads mapped against the reference (0 to 1).
+        :param reference_id: Unique string id of the reference database.
+        :param reference_name: Human-readable name of the reference database.
+        :param sample_id: Unique string id of the sample.
+        :param sample_filename: Name of the uploaded sample.
+            By default, this should be the same as the name of the uploaded FASTA or FASTQ file.
+        :return: A new instance of Analysis.
         """
-        self._id = the_id
+        self.id = the_id
+        self.status = status
+        self.number_reads = number_reads
+        self.percentage_mapped = proportion_mapped
+        self.reference_id = reference_id
+        self.reference_name = reference_name
+        self.sample_id = sample_id
+        self.sample_filename = sample_filename
 
     def get_table(self):
         """
         Returns an ordered list of the top hits found for a given Sample against a given
         ReferenceDatabase (per read or contig).
         """
-        table_url = self._url_builder.get_resource_url(id=self._id, action=u"table")
+        table_url = self._url_builder.get_resource_url(id=self.id, action=u"table")
         request = OneCodexRequest.get(table_url)
         return request.json()
 
@@ -44,7 +62,7 @@ class Analysis(object):
         :param chunk_size: The size of the chunks in which the file will be downloaded.
         :return: Yields each of the file chunks using a generator.
         """
-        raw_data_url = self._url_builder.get_resource_url(id=self._id, action="raw")
+        raw_data_url = self._url_builder.get_resource_url(id=self.id, action=u"raw")
         r = OneCodexRequest.get(raw_data_url, stream=True)
         for chunk in r.iter_content(chunk_size):
             yield chunk
--- test/v0/analyses/TestAnalysis.py
+++ test/v0/analyses/TestAnalysis.py
@@ -11,15 +11,18 @@ from v0.analyses.Analysis import Analysis
 
 class TestAnalysis(unittest.TestCase):
 
+    def _get_dummy_analysis_instance(self):
+        return Analysis(u"3186542a03c74205", None, None, None, None, None, None, None)
+
     def test_get_table(self):
-        analysis = Analysis(u"3186542a03c74205")
+        analysis = self._get_dummy_analysis_instance()
         table_results = analysis.get_table()
         self.assertIsInstance(table_results, list)
         self.assertEqual(len(table_results), 125)
 
     def test_save_raw_data_to_path(self):
         destination_path = u"/Users/jc/Desktop/ocanalysis.tsv.gz"
-        analysis = Analysis(u"3186542a03c74205")
+        analysis = self._get_dummy_analysis_instance()
         analysis.download_and_save_raw_data_to_path(destination_path)
         self.assertTrue(path.isfile(destination_path))
         file_stat_info = stat(destination_path)
